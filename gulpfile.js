0f520d9031217a2b4eb00c3f8a9a56eab141bfc4
kroyo <kroyoshi@163.com>
2018-05-15 18:18:30 +0800

    构建gulp配置

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/**
+ * gulpfile 配置文件
+ * kroyo
+ * version   v1.0.0
+ * gulp v3.9.1
+ */
+
+
+var gulp = require('gulp');
+
+gulp.task('clean',function(){
+	return gulp.src([],{ read:false })
+});
